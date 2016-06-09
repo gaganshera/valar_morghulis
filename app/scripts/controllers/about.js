0819443e83846335cfe6569360a7661c4ff60c76
Gaganjot Singh <gaganjot@MP097VJ3.(none)>
2016-06-09 14:42:55 +0530

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name valarMorghulisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the valarMorghulisApp
+ */
+angular.module('valarMorghulisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name valarMorghulisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the valarMorghulisApp
+ */
+angular.module('valarMorghulisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
